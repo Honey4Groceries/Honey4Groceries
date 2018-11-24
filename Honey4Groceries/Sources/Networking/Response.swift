27087facc61627b0b5c60458c90b450bf2931e19
gypang <gypang24@gmail.com>
2018-11-24 11:25:48 -0800

    Adds Response class and corresponding protocol

---
+++ Honey4Groceries/Sources/Networking/Response.swift
@@ -0,0 +1,37 @@
+//
+//  Response.swift
+//  Honey4Groceries
+//
+//  Created by Godwin Pang on 11/24/18.
+//
+
+import Foundation
+import SwiftyJSON
+import Alamofire
+
+public class Response: ResponseProtocol {
+    
+    /// Type of Result
+    public let type: Result 
+    
+    /// HTTP Response
+    public let httpResponse: HTTPURLResponse?
+    
+    /// HTTP Response Status Code
+    public var httpStatusCode: Int? {
+        return self.type.code
+    }
+    
+    /// Raw Data of Response
+    public var rawData: Data?
+    
+    /// JSON Data of Response
+    public lazy var jsonData: JSON = try! JSON(data: rawData ?? Data())
+    
+    /// Initialize
+    public init(afResponse response: DefaultDataResponse, request: ResponseProtocol){
+        self.type = Result.from(response: response.response)
+        self.httpResponse = response.response
+        self.rawData = response.data
+    }
+}
