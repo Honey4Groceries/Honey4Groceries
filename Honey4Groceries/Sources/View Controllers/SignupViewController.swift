6615d19d63d36ed93214c67e5a4820bcce439dda
Junhong Pan <panjunhong05@gmail.com>
2018-12-04 17:01:28 -0800

    Added SignupViewController

---
+++ Honey4Groceries/Sources/View Controllers/SignupViewController.swift
@@ -0,0 +1,31 @@
+//
+//  SignupViewController.swift
+//  Honey4Groceries
+//
+//  Created by Paul Pan on 2018/12/04.
+//
+
+import UIKit
+import PureLayout
+
+class SignupViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.backgroundColor = .white
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
