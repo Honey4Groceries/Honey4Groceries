3d134f8482e8811e21effce95b07a605ebe1cc88
Junhong Pan <panjunhong05@gmail.com>
2019-01-26 13:56:00 -0800

    Renamed location to locationService to follow naming convention

--- Honey4Groceries/Sources/View Controllers/ViewController.swift
+++ Honey4Groceries/Sources/View Controllers/ViewController.swift
@@ -9,13 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    let location = LocationService()
+    let locationService = LocationService()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         print("Hello")
-        self.location.getLocation().done { currentLocation in
+        self.locationService.getLocation().done { currentLocation in
             print(currentLocation.debugDescription)
         }
     }
