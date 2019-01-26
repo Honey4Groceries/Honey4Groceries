d5b07f634414591acef86f6782b39c71a94dd7e5
Junhong Pan <panjunhong05@gmail.com>
2019-01-26 10:52:32 -0800

    Cleaned up location service

--- Honey4Groceries/Sources/Services/LocationService.swift
+++ Honey4Groceries/Sources/Services/LocationService.swift
@@ -1,55 +1,18 @@
-import CoreLocation
 import PromiseKit
 
-class LocationService: NSObject, CLLocationManagerDelegate {
-    
-    var location: CLLocation?
-    var retrieved: Date?
-    var locationManager: CLLocationManager
-    
-    public override init() {
-        self.location = nil
-        self.locationManager = CLLocationManager()
-        self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
-        self.retrieved = Date()
-        super.init()
-        
-        locationManager.delegate = self
-    }
-    
-    func requestAuth() {
-        locationManager.requestAlwaysAuthorization()
-    }
-    
-    func requestLocation() {
-        locationManager.requestLocation()
-    }
-
+class LocationService {
     func getLocation() -> Guarantee<CLLocation?> {
-        
         let guarantee = Guarantee<CLLocation?> {seal in
-            CLLocationManager.requestAuthorization(type: .always).then { CLAuthorizationStatus -> Promise<[CLLocation]> in
-                return CLLocationManager.requestLocation()
-            }.done {currentLocation in
-                seal(currentLocation.first)
-            }.catch {error in
-                print("Get Location Failed!")
+            firstly {
+                CLLocationManager.requestAuthorization(type: .always)
+                }.then { authorizationStatus -> Promise<[CLLocation]> in
+                    return CLLocationManager.requestLocation()
+                }.done {currentLocation in
+                    seal(currentLocation.first)
+                }.catch {error in
+                    print("Get Location Failed!")
             }
         }
-        
         return guarantee
-    }
-
-    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
-        if let error = error as? CLError, error.code == .denied {
-            manager.stopUpdatingLocation()
-            return
-        }
-    }
-
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        if locations.first != nil {
-            location = locations.first
-        }
     }
 }
--- Honey4Groceries/Sources/View Controllers/ViewController.swift
+++ Honey4Groceries/Sources/View Controllers/ViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    let location = LocationServiceTest()
+    let location = LocationService()
 
     override func viewDidLoad() {
         super.viewDidLoad()
