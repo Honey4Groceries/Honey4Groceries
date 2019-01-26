aefea2a34e70fe0dad5f27aec8a864af12b9468a
Junhong Pan <panjunhong05@gmail.com>
2019-01-26 10:56:03 -0800

    Fixed indentation

--- Honey4Groceries/Sources/Services/LocationService.swift
+++ Honey4Groceries/Sources/Services/LocationService.swift
@@ -5,12 +5,12 @@ class LocationService {
         let guarantee = Guarantee<CLLocation?> {seal in
             firstly {
                 CLLocationManager.requestAuthorization(type: .always)
-                }.then { authorizationStatus -> Promise<[CLLocation]> in
-                    return CLLocationManager.requestLocation()
-                }.done {currentLocation in
-                    seal(currentLocation.first)
-                }.catch {error in
-                    print("Get Location Failed!")
+            }.then { authorizationStatus -> Promise<[CLLocation]> in
+                return CLLocationManager.requestLocation()
+            }.done {currentLocation in
+                seal(currentLocation.first)
+            }.catch {error in
+                print("Get Location Failed!")
             }
         }
         return guarantee
