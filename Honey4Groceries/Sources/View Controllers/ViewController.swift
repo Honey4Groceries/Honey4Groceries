714d2ec6987ce5593e33b79c8e687209f844b2ad
Junhong Pan <panjunhong05@gmail.com>
2019-02-02 11:56:55 -0800

    Error handling of getLocation in the view controller

--- Honey4Groceries/Sources/View Controllers/ViewController.swift
+++ Honey4Groceries/Sources/View Controllers/ViewController.swift
@@ -6,17 +6,33 @@
 //
 
 import UIKit
+import PromiseKit
 
 class ViewController: UIViewController {
 
     let locationService = LocationService()
+    
+    var latitude: Double?
+    var longitude: Double?
+    var locationError: Error?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        print("Hello")
         self.locationService.getLocation().done { currentLocation in
-            print(currentLocation.debugDescription)
+            self.latitude = currentLocation?.coordinate.latitude
+            self.longitude = currentLocation?.coordinate.longitude
+            print(self.latitude as Any)
+            print(self.longitude as Any)
+        }.catch { error in
+            switch error {
+            case CLLocationManager.PMKError.notAuthorized:
+                print("Not Authorized")
+            default:
+                print("Other Errors")
+            }
+            self.locationError = error
+            print(self.locationError as Any)
         }
     }
 }
